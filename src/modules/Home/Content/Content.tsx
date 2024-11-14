793c1d5f83a84ee27171b94cc43a0f85caf309a1
Merekeyev Dias <dikxarper.programming@gmail.com>
2024-11-14 15:12:14 +0500

    fixed menu-header height bug

--- src/modules/Home/Content/Content.tsx
+++ src/modules/Home/Content/Content.tsx
@@ -1,6 +1,3 @@
-import { useSelector } from 'react-redux';
-import { RootState } from '@/app/store/store';
-
 import TitlePage from './FrontMatter/TitlePage';
 import CopyrigthPage from './FrontMatter/CopyrightPage';
 import PartialSpacing from '@/shared/components/Content/PartialSpacing';
@@ -8,10 +5,9 @@ import Body from './Body/Body';
 import styles from './Content.module.scss';
 
 const Content: React.FC = () => {
-    const contentHeight = useSelector((state: RootState) => state.menuSlice.contentHeight)
-    
+
     return (
-        <div className={styles.content} style={{height: `${contentHeight}vh`}}> 
+        <div className={styles.content}>
             <div className={styles.content_inner}>
                 <PartialSpacing />
                 <TitlePage />
